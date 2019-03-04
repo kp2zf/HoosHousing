33dddacdb8b315e33826a7ec870da1abb40e34de
Jack Morris <jxmorris12@gmail.com>
2019-03-03 23:43:46 -0500

    default test cases for Travis

--- housing/tests.py
+++ housing/tests.py
@@ -1,3 +1,43 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Building
+
+''' Begin models test cases. '''
+
+class ModelTests(TestCase):
+	def test_building_exists(self):
+		building = create_building()
+		self.assertIsNotNone(building)
+
+''' Begin forms test cases. '''
+
+''' Begin views test cases. '''
+
+class HomeTestCases(TestCase):
+	def test_empty_home(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'No buildings found')
+
+class BuildingDetailViewTests(TestCase):
+	def test_future_question(self):
+		"""
+		The detail view of a question with a pub_date in the future
+		returns a 404 not found.
+		"""
+		building = create_building()
+		building.save()
+		url = reverse('housing:building_detail', args=(building.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, building.name)
+		self.assertContains(response, building.address)
+
+''' Helper methods for tests. '''
+
+def create_building():
+	return Building(
+		name='Grandmarc', 
+		address='301 15th St NW, Charlottesville, VA 22903'
+	)
